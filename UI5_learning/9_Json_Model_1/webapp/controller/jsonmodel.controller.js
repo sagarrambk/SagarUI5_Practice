077f7c76f25137a93e81c2ccf6b529fbae4727fc
Sagar Ram <sagarram.bk@gmail.com>
2022-06-08 10:24:32 +0000

    JSONmodel_1

---
+++ UI5_learning/9_Json_Model_1/webapp/controller/jsonmodel.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
+    'use strict';
+    return Controller.extend("home.controller.jsonmodel", {
+        onInit: function () {
+            // here we are creating JSON payload 
+            var oJsonData = {
+
+                "Person": {
+                    "Name": "Sagar",
+                    "Id": "007",
+                    "City": "Hyderabad",
+                    "Pin": "500000",
+                    "Contact": "123456"
+                }
+            }
+            // now we need to creae model and link above payload to the model
+            var oJsonModel = new JSONModel(oJsonData);
+            // link model datat to the view
+            this.getView().setModel(oJsonModel);
+        },
+    });
+});
